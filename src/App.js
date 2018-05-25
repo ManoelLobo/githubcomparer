a85c1dcf8bd77d439dd6e9d89eb846d7aff7d1ae
Manoel Lobo <manoel.lobo@gmail.com>
2018-05-24 21:35:39 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>GitHub Comparer</h1>
+    </header>
+  </div>
+);
+
+export default App;
